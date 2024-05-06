051d8c0c9d4188ea0cf5e5d2ec5f6fe04507803a
Hammad Saleem <112611922+hammadshub@users.noreply.github.com>
2024-05-05 19:40:52 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home/>}/>
+          <Route path="/." element={<Home/>}/>
           <Route path="/orderbox" element={<OrderBox/>}/>
           <Route path="/faq" element={<Faq/>}/>
           <Route path="/contact" element={<Contact/>}/>
