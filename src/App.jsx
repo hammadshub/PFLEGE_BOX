6e32b63c287b12301d6a9d21737ac536d5bcf797
hammadshub <hammadsweb@gmail.com>
2024-05-08 21:03:08 -0800

    careBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home/Home";
-import OrderBox from "./Pages/OrderBox";
+import OrderBox from "./Pages/orderBox/OrderBox";
 import Faq from "./Pages/Faq";
 import Contact from "./Pages/Contact";
 import Header from "./components/header/Header";
---
+++ src/Pages/orderBox/OrderBox.jsx
@@ -0,0 +1,219 @@
+import React,{useState} from "react";
+import "./style.css";
+ 
+
+
+
+const OrderBox = () => {
+
+  const [progress, setProgress] = useState(0);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const handleIncrementClick = () => {
+    if (progress < 100) {
+      setProgress(progress + 10);
+      if (progress >= 40) {
+        setButtonDisabled(true);  
+      }
+    }
+  };
+
+  const handleDecrementClick = () => {
+    if (progress > 0) {
+      setProgress(progress - 10);
+      if (progress <= 50) {
+        setButtonDisabled(false);  
+      }
+    }
+  };
+
+
+
+
+  return (
+    
+    
+    <div className="ordr_container">
+      <div className="ordr_content">
+        <div className="heading">
+          <h1>
+            Customize Your <span>Care Box</span>
+          </h1>
+          <img src="./images/line.png" alt="" />
+        </div>
+
+
+        <div className="bar"><progress id="file" value={progress} max="100">70%</progress></div>
+
+        <div className="bar_content">
+          <div className="left_bar">
+               <div className="left_img">
+              <img src="./images/bar.png" alt="" />
+            </div>
+            <div className="left_cont">Bed Protectors</div>
+          </div>
+
+          <div className="right_bar">
+            <div className="count_bar">
+              <div className="plus">
+              <button onClick={handleIncrementClick} disabled={buttonDisabled}> <img src="./images/plus.png" alt="+" /></button>
+              </div>
+
+              <div className="count">{progress}</div>
+              <div className="minus">
+               <button onClick={handleDecrementClick}><img src="./images/minus.png" alt="-" /></button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div className="bar_content">
+          <div className="left_bar">
+               <div className="left_img">
+              <img src="./images/bar.png" alt="" />
+            </div>
+            <div className="left_cont">Bed Protectors</div>
+          </div>
+
+          <div className="right_bar">
+            <div className="count_bar">
+              <div className="plus">
+              <button onClick={handleIncrementClick} disabled={buttonDisabled}> <img src="./images/plus.png" alt="+" /></button>
+              </div>
+
+              <div className="count">{progress}</div>
+              <div className="minus">
+               <button onClick={handleDecrementClick}><img src="./images/minus.png" alt="-" /></button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        
+        <div className="bar_content">
+          <div className="left_bar">
+               <div className="left_img">
+              <img src="./images/bar.png" alt="" />
+            </div>
+            <div className="left_cont">Bed Protectors</div>
+          </div>
+
+          <div className="right_bar">
+            <div className="count_bar">
+              <div className="plus">
+              <button onClick={handleIncrementClick} disabled={buttonDisabled}> <img src="./images/plus.png" alt="+" /></button>
+              </div>
+
+              <div className="count">{progress}</div>
+              <div className="minus">
+               <button onClick={handleDecrementClick}><img src="./images/minus.png" alt="-" /></button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        
+        <div className="bar_content">
+          <div className="left_bar">
+               <div className="left_img">
+              <img src="./images/bar.png" alt="" />
+            </div>
+            <div className="left_cont">Bed Protectors</div>
+          </div>
+
+          <div className="right_bar">
+            <div className="count_bar">
+              <div className="plus">
+              <button onClick={handleIncrementClick} disabled={buttonDisabled}> <img src="./images/plus.png" alt="+" /></button>
+              </div>
+
+              <div className="count">{progress}</div>
+              <div className="minus">
+               <button onClick={handleDecrementClick}><img src="./images/minus.png" alt="-" /></button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        
+        <div className="bar_content">
+          <div className="left_bar">
+               <div className="left_img">
+              <img src="./images/bar.png" alt="" />
+            </div>
+            <div className="left_cont">Bed Protectors</div>
+          </div>
+
+          <div className="right_bar">
+            <div className="count_bar">
+              <div className="plus">
+              <button onClick={handleIncrementClick} disabled={buttonDisabled}> <img src="./images/plus.png" alt="+" /></button>
+              </div>
+
+              <div className="count">{progress}</div>
+              <div className="minus">
+               <button onClick={handleDecrementClick}><img src="./images/minus.png" alt="-" /></button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+        <div className="bar_content">
+          <div className="left_bar">
+               <div className="left_img">
+              <img src="./images/bar.png" alt="" />
+            </div>
+            <div className="left_cont">Bed Protectors</div>
+          </div>
+
+          <div className="right_bar">
+            <div className="count_bar">
+              <div className="plus">
+              <button onClick={handleIncrementClick} disabled={buttonDisabled}> <img src="./images/plus.png" alt="+" /></button>
+              </div>
+
+              <div className="count">{progress}</div>
+              <div className="minus">
+               <button onClick={handleDecrementClick}><img src="./images/minus.png" alt="-" /></button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+        <div className="bar_content">
+          <div className="left_bar">
+               <div className="left_img">
+              <img src="./images/bar.png" alt="" />
+            </div>
+            <div className="left_cont">Bed Protectors</div>
+          </div>
+
+          <div className="right_bar">
+            <div className="count_bar">
+              <div className="plus">
+              <button onClick={handleIncrementClick} disabled={buttonDisabled}> <img src="./images/plus.png" alt="+" /></button>
+              </div>
+
+              <div className="count">{progress}</div>
+              <div className="minus">
+               <button onClick={handleDecrementClick}><img src="./images/minus.png" alt="-" /></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+        
+      </div>
+    </div>
+  
+  );
+};
+
+export default OrderBox;
